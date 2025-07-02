c4e9ee03cc7dc4129dd99f3cd2bbdc28de6bc53f
Mike-Justine <mikejustine529@gmail.com>
2025-07-02 16:45:30 +0300

    Delete JobTech directory

--- JobTech/includes/header.php
+++
@@ -1,4 +0,0 @@
-<!-- Simple Header Placeholder -->
-<header>
-  <h1>JobTech Admin Dashboard</h1>
-</header>
--- JobTech/script.js
+++
@@ -1,5 +0,0 @@
-js
-function showform(formId) {
-    document.querySelectorAll(".form-box").forEach (form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
